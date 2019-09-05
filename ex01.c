e638f185d74e183a06d40fb584dc7f7382d17c84
Francisco Geneuto R. Júnior <38793083+geneuto@users.noreply.github.com>
2019-09-05 16:45:26 -0300

    Add files via upload

--- ex01.c
+++ ex01.c
@@ -1,6 +1,8 @@
-//
+//  Professor: Marcos Candia
 //  Francisco Geneuto - RA: 327485211919
-//  Ciência da Computação 4ª série
+//  Ciencia da Computacao 4ª serie
+//  Faculdade Anhanguera - São José dos Campos - SP
+//  05 de setembro de 2019
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,19 +13,19 @@
 // ==========================================
 void exercicio01(){
     
-    // Váriaveis
+    // Variaveis
     float celsius;
     
     // Conteudo
-    printf("Bem vindo ao exercício numero 1 da primeira lista de exercicios do professor Marcos Candia.\n");
-    printf("01) Ler uma temperatura em graus Celsius e apresentá-la convertida\n em graus Fahrenheit. A fórmula de conversão é: F ← (9 * C + 160) / 5, sendo\n F a temperatura em Fahrenheit e C a temperatura em Celsius.\n\n");
+    printf("Bem vindo ao exercicio numero 1 da primeira lista de exercicios do professor Marcos Candia.\n");
+    printf("01) Ler uma temperatura em graus Celsius e apresenta-la convertida\n em graus Fahrenheit. A formula de conversao e: F ← (9 * C + 160) / 5, sendo\n F a temperatura em Fahrenheit e C a temperatura em Celsius.\n\n");
     printf("Digite o valor da temperatura em graus Celsius a ser convertida: ");
     scanf("%f", &celsius);
     
     float fahrenheit = (9 * celsius + 160)/5;
     
     // Resultado
-    printf("O valor convertido é de: %.2f º Fahrenheit", fahrenheit);
+    printf("O valor convertido e de: %.2f º Fahrenheit", fahrenheit);
     system("pause");
     
     
@@ -39,28 +41,28 @@ void exercicio02(){
     int consumo = 12;
     
     // Conteudo
-    printf("\n\nBem vindo ao exercício numero 4 da primeira lista de exercicios do professor Marcos Candia.\n\n");
-    printf("02) Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando\n"
-           "um automóvel que faz 12 quilômetros por litro. Para obter o cálculo, o usuário deve fornecer o\n"
-           "tempo gasto (variável TEMPO) e a velocidade média (variável VELOCIDADE) durante a viagem.\n"
-           "Desta forma, será possível obter a distância percorrida com a fórmula: DISTÂNCIA ← TEMPO *\n"
+    printf("\n\nBem vindo ao exercicio numero 4 da primeira lista de exercicios do professor Marcos Candia.\n\n");
+    printf("02) Efetuar o calculo da quantidade de litros de combustivel gasta em uma viagem, utilizando\n"
+           "um automovel que faz 12 quilômetros por litro. Para obter o calculo, o usuario deve fornecer o\n"
+           "tempo gasto (variavel TEMPO) e a velocidade media (variavel VELOCIDADE) durante a viagem.\n"
+           "Desta forma, sera possivel obter a distância percorrida com a formula: DISTÂNCIA ← TEMPO *\n"
            "VELOCIDADE. A partir do valor da distância, basta calcular a quantidade de litros de\n"
-           "combustível utilizada na viagem com a fórmula: LITROS_USADOS ← DISTÂNCIA / 12. O\n"
-           "programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância\n"
+           "combustivel utilizada na viagem com a formula: LITROS_USADOS ← DISTÂNCIA / 12. O\n"
+           "programa deve apresentar os valores da velocidade media, tempo gasto na viagem, a distância\n"
            "percorrida e a quantidade de litros utilizada na viagem.\n\n");
     
     printf("Digite o tempo da viagem em (horas): ");
     scanf("%f", &tempo_gasto);
     
-    printf("\nDigite a velocidade média da viagem (Km/h): ");
+    printf("\nDigite a velocidade media da viagem (Km/h): ");
     scanf("%f", &velocidade);
     
     // Resultado
     float distancia_percorrida = tempo_gasto * velocidade;
     float litros = distancia_percorrida / consumo;
     
-    printf("A distancia percorrida pelo carro é de: %.2f Km.", distancia_percorrida);
-    printf("\nA quantidade de litros gasto é de: %.2f litros.\n", litros);
+    printf("A distancia percorrida pelo carro e de: %.2f Km.", distancia_percorrida);
+    printf("\nA quantidade de litros gasto e de: %.2f litros.\n", litros);
     
     system("pause");
     
@@ -78,16 +80,16 @@ void exercicio03(){
     int i;
     
     // Conteudo
-    printf("\n\nBem vindo ao exercício numero 4 da segunda lista de exercicios do professor Marcos Candia.\n\n");
+    printf("\n\nBem vindo ao exercicio numero 4 da segunda lista de exercicios do professor Marcos Candia.\n\n");
     printf("03) Ler os valores de quatro notas escolares bimestrais de um aluno representadas\n"
-           "pelas variáveis N1, N2, N3 e N4. Calcular a média aritmética (variável MD1) desse aluno\n"
-           "e apresentar a mensagem ""Aprovado"" se a média obtida for maior ou igual a 7; caso\n"
-           "contrário, o programa deve solicitar a quinta nota (nota de exame, representada pela\n"
-           "variável NE) do aluno e calcular uma nova média aritmética (variável MD2) entre a nota\n"
-           "de exame e a primeira média aritmética. Se o valor da nova média for maior ou igual a\n"
-           "cinco, apresentar a mensagem ""Aprovado em exame""; caso contrário, apresentar a\n"
-           "mensagem ""Reprovado"". Informar também, após a apresentação das mensagens, o\n"
-           "valor da média obtida pelo aluno.\n\n");
+           "pelas variaveis N1, N2, N3 e N4. Calcular a media aritmetica (variavel MD1) desse aluno\n"
+           "e apresentar a mensagem ""Aprovado"" se a media obtida for maior ou igual a 7; caso\n"
+           "contrario, o programa deve solicitar a quinta nota (nota de exame, representada pela\n"
+           "variavel NE) do aluno e calcular uma nova media aritmetica (variavel MD2) entre a nota\n"
+           "de exame e a primeira media aritmetica. Se o valor da nova media for maior ou igual a\n"
+           "cinco, apresentar a mensagem ""Aprovado em exame""; caso contrario, apresentar a\n"
+           "mensagem ""Reprovado"". Informar tambem, apos a apresentacao das mensagens, o\n"
+           "valor da media obtida pelo aluno.\n\n");
     
     for(i=1; i <= 4; i++){
         printf("\nDigite a %iª nota:\n", i);
@@ -95,24 +97,24 @@ void exercicio03(){
         NGERAL += N[i]; // Calcula as 4 notas
     }
     
-    MD1 = NGERAL/4; // Calcula a média 1 sem exame
+    MD1 = NGERAL/4; // Calcula a media 1 sem exame
     
     // Resultado
     if (MD1 > 6){
         printf("Aluno APROVADO\n");
-        printf("Sua média foi de %.2f", MD1);
+        printf("Sua media foi de %.2f", MD1);
     }else{
         printf("\nDigite a nota do Exame:\n");
         scanf("%f", &NE);
         
-        float MD2 = (MD1 + NE)/2; // Calcula média 2 com exame
+        float MD2 = (MD1 + NE)/2; // Calcula media 2 com exame
         
         if(MD2 < 5){
             printf("Aluno REPROVADO\n");
-            printf("Sua média foi de %.2f\n", MD2);
+            printf("Sua media foi de %.2f\n", MD2);
         }else{
             printf("Aluno APROVADO COM EXAME\n");
-            printf("Sua média foi de %.2f\n", MD2);
+            printf("Sua media foi de %.2f\n", MD2);
         }
     }
     
@@ -128,9 +130,9 @@ void exercicio04(){
     int i;
     int soma = 0;
     // Conteudo
-    printf("\n\nBem vindo ao exercício numero 14 da segunda lista de exercicios do professor Marcos Candia.\n\n");
-    printf("04) Efetuar a leitura de três valores inteiros desconhecidos representados pelas\n"
-           "variáveis A, B e C. Somar os valores fornecidos e apresentar o resultado somente se\n"
+    printf("\n\nBem vindo ao exercicio numero 14 da segunda lista de exercicios do professor Marcos Candia.\n\n");
+    printf("04) Efetuar a leitura de tres valores inteiros desconhecidos representados pelas\n"
+           "variaveis A, B e C. Somar os valores fornecidos e apresentar o resultado somente se\n"
            "for maior ou igual a 100.\n\n");
     for (i=0; i<3; i++){
         printf("Insira um valor qualquer:\n");
@@ -139,9 +141,9 @@ void exercicio04(){
     }
     
     if (soma >= 100){
-        printf("A soma dos valores inseridos é: %d\n\n", soma);
+        printf("A soma dos valores inseridos e: %d\n\n", soma);
     }else{
-        printf("A soma dos valores inseridos é menor que 100!\n\n");
+        printf("A soma dos valores inseridos e menor que 100!\n\n");
     }
     
 }
@@ -157,16 +159,16 @@ void exercicio05(){
     float total;
     
     // Conteudo
-    printf("\n\nBem vindo ao exercício numero 4 da terceira lista de exercicios do professor Marcos Candia.\n\n");
-    printf("\n05) O cardápio de uma lanchonete é o seguinte:\n\n"
-           "ESPECIFICAÇÃO        CÓDIGO      PREÇO\n"
+    printf("\n\nBem vindo ao exercicio numero 4 da terceira lista de exercicios do professor Marcos Candia.\n\n");
+    printf("\n05) O cardapio de uma lanchonete e o seguinte:\n\n"
+           "ESPECIFICAcaO        CoDIGO      PREcO\n"
            "Cachorro quente         100       1.20\n"
            "Bauru simples           101       1.30\n"
            "Bauru com ovo           102       1.50\n"
-           "Hambúrger               103       1.20\n"
+           "Hamburger               103       1.20\n"
            "Cheeseburguer           104       1.30\n\n"
-           "Implemente um programa que leia o código do item pedido, a quantidade e calcule o\n"
-           "valor a ser pago por aquele lanche. Considere que a cada execução somente será\n"
+           "Implemente um programa que leia o codigo do item pedido, a quantidade e calcule o\n"
+           "valor a ser pago por aquele lanche. Considere que a cada execucao somente sera\n"
            "calculado um item.\n\n");
     printf("\n\n Escolha um item do cardapio inserindo seu numero correspondente:\n");
     scanf("%i", &cardapio);
@@ -194,7 +196,7 @@ void exercicio05(){
             break;
             
         case 103:
-            printf("Insira a quantidade de Hambúrger desejado: ");
+            printf("Insira a quantidade de Hamburger desejado: ");
             scanf("%i", &qnt);
             total = qnt * preco[3];
             printf("\nSeu pedido ficou no total de: R$ %.2f", total);
@@ -208,7 +210,7 @@ void exercicio05(){
             break;
             
         default:
-            printf("\nItém não encontrado no cardápio");
+            printf("\nItem nao encontrado no cardapio");
             break;
     }
     
@@ -222,34 +224,184 @@ void exercicio06(){
     int n;
     int i;
     // Conteudo
-    printf("\n\nBem vindo ao exercício numero 1 da quarta lista de exercicios do professor Marcos Candia.\n\n");
-    printf("06) Elaborar um programa que apresente como resultado os quadrados dos números\n"
+    printf("\n\nBem vindo ao exercicio numero 1 da quarta lista de exercicios do professor Marcos Candia.\n\n");
+    printf("06) Elaborar um programa que apresente como resultado os quadrados dos numeros\n"
            "inteiros existentes na faixa de valores de 15 a 200.\n\n");
     
     for (i=15;i < 201; i++){
         n = i*i;
-        printf("O quadrado do número %i é %i\n", i, n);
+        printf("O quadrado do numero %i e %i\n", i, n);
     }
 }
 
 // =======================================
 // == Setimo exercicio - sexta lista ==
 // =======================================
-int exercicio07(){
+// Funcao
+int fib(int n);
+void exercicio07(){
 
+        // Variaveis
+        int n = 0;
+        
+        // Conteudo
+        printf("\n\nBem vindo ao exercicio numero 3 da sexta lista de exercicios do professor Marcos Candia.\n\n");
+        printf("07) Faca uma funcao recursiva que calcule e retorne o N-esimo termo da sequencia"
+               "Fibonacci. Alguns numeros desta sequencia sao: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...");
+        
+        printf("Insira um valor da posicao:\n");
+        scanf("%i", &n);
+        
+        // Resultado
+        printf("O termo da sequencia de Fibonacci e: %d\n", fib(n));
+        system("pause");
+}
+// Funcao recursiva de fibonacci
+int fib(int n) {
+    if(n == 1) {
+        return 0;
+    } else if(n == 2) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
 }
 
 // =======================================
 // == Oitavo exercicio - terceira lista ==
 // =======================================
 void exercicio08(){
+    // Variaveis
+    int vetorA[10], numeroX = 0, resetar = 0, maior, c, x;
+    
+    // Conteudo
+    printf("\n\nBem vindo ao exercicio numero 7 da setima lista de exercicios do professor Marcos Candia.\n\n");
+    printf("08) Considere um vetor de 10 numeros inteiros positivos maiores que zero e\n"
+           "um unico numero X inteiro, tambem positivo e maior que zero. Faça um\n"
+           "programa para:\n\n"
+           "• ler pelo teclado o vetor;\n"
+           "• ler pelo teclado o numero X;\n"
+           "• encontrar e escrever a posicao;\n"
+           "• dizer quantos numeros no vetor sao maiores que X e menores que\n"
+           "X.\n\n");
+    
+    // Resultado
+    do{
+        printf("Digite um valor para X que seja maior que 0 \n"); // Le o valor de X
+        scanf("%i", &numeroX);
+    }while(numeroX == 0 || numeroX < 0);
+    
+    printf("\nDigite o valor a ser inserido nas posicoes do vetor ate 10: \n\n");
+    
+    // Le o valor do vetor
+    for (int contador = 0; contador < 10; contador++ ){
+        int armazenado;
+        printf("Digite o %i º valor a ser armazenado\n", contador);
+        scanf("%i", &armazenado);
+        if(armazenado < 1){
+            printf("\nATENCAO: Valor digitado menor que 1, favor digitar um numero maior que 0\n\n");
+            vetorA[contador] = resetar;
+            contador = resetar;
+        }else{
+            vetorA[contador] = armazenado;
+        }
+    }
+    
+    // Imprime o vetor na tela
+    for(int i = 0; i < 10; i++){
+        printf("\n\nO vetor da posicao [%i] esta com os numeros %d armazenados.\n", i, vetorA[i]);
+    }
+    
+    // Diz quantos numeros sao maiores que X
+    x = vetorA[0];
+    c = 0;
+    maior = 0;
+    while(c < 10){
+        if (vetorA[c] > numeroX){
+            maior++;
+        }
+        c++;
+    }
+    if (maior == 0){
+        printf("Nenhum numero e maior que X\n");
+    }else{
+        printf("A quantidade de numeros maior que X e: %i\n", maior);
+    }
+    
     
 }
+
+// =====================================
+// == Nove exercicio - terceira lista ==
+// =====================================
 void exercicio09(){
-    
+    // Variaveis
+    int matrizA[4][4], matrizB[4][4];
+    // Conteudo
+    printf("\n\nBem vindo ao exercicio numero 1 da oitava lista de exercicios do professor Marcos Candia.\n\n");
+    printf("09) Elabore um algoritmo que leia aleatoriamente uma matriz quadrada A 4x4.\n"
+           "A partir desta, construir uma matriz B de mesma ordem, onde cada\n"
+           "elemento devera ser o dobro do elemento correspondente da matriz A.\n\n\n");
+    for(int i = 0; i < 4; i++){
+        matrizA[i][i] = i;
+        if (matrizA[i][i] == 0){
+            matrizA[i][i] = 10;
+            printf("\nA matriz A[%d][%d] contem: %d",i,i, matrizA[i][i]);
+        }else{
+        printf("\nA matriz A[%d][%d] contem: %d",i,i, matrizA[i][i]);
+        }
+    }
+    printf("\n\n");
+    for (int i = 0; i < 4; i++){
+        matrizB[i][i] = matrizA[i][i] * 2;
+        printf("A matriz B[%d][%d] contem: %d\n",i,i, matrizB[i][i]);
+    }
 }
+
+// ====================================
+// == Dez exercicio - terceira lista ==
+// ====================================
 void exercicio10(){
+    // Variaveiso
+    int matriz[3][3], vetorCOL[3], soma = 0, i, j;
+    // Conteudo
+    printf("\n\nBem vindo ao exercicio numero 6 da oitava lista de exercicios do professor Marcos Candia.\n\n");
+    printf("10) Faça um programa que permita ao usuario entrar com uma matriz de 3x3\n"
+           "numeros inteiros. Em seguida, gere um vetor pela soma dos numeros\n"
+           "de cada coluna da matriz e mostrar na tela esse vetor. Por exemplo, a\n"
+           "matriz:\n\n"
+           "5   -8     10\n"
+           "1    2     15\n"
+           "25   10    7\n\n"
+           "Vai gerar um vetor, onde cada posiçao e a soma das colunas da matriz.\n"
+           "A primeira posiçao sera 5 + 1 + 25, e assim por diante. Por fim, o programa\n"
+           "devera imprimir o seguinte vetor:\n\n"
+           "31   4     3");
+    for (i=0; i < 3; i++){
+        for(j=0; j < 3; j++){
+             printf("\nEscreva o valor da posicao da Matriz [%d] [%d]\n", i, j);
+             scanf("%d", &matriz[i][j]);
+        }
+    }
+    // Imprime a Matriz na tela
+    printf("\n=================================\n");
+    printf("======= A MATRIZ DIGITADA =======\n");
+    printf("=================================\n");
+    printf("\n [%d]         [%d]        [%d]",matriz[0][0], matriz[0][1],matriz[0][2]);
+    printf("\n [%d]         [%d]        [%d]",matriz[1][0], matriz[1][1],matriz[1][2]);
+    printf("\n [%d]         [%d]        [%d]",matriz[2][0], matriz[2][1],matriz[2][2]);
     
+    // Soma dos valores de cada coluna
+    printf("\n\n==================================\n");
+    printf("======= A SOMA DAS COLUNAS =======\n");
+    printf("==================================\n");
+    for (j = 0; j < 3; j++){
+        for (i = 0; i < 3; i++){
+            soma = soma + matriz[i][j];
+        }
+        vetorCOL [j] = soma;
+    }
+    printf("\n [%d]           [%d]          [%d]\n\n",vetorCOL[0], vetorCOL[1],vetorCOL[2]);
 }
 int main() {
     
@@ -274,19 +426,19 @@ int main() {
     printf("==================================================\n");
     printf("====================== MENU ======================\n");
     printf("==================================================\n");
-    printf("== 1 p/ Conversão de graus                      ==\n");
-    printf("== 2 p/ Consumo de combustivel                  ==\n");
-    printf("== 3 p/ Calcular média do aluno                 ==\n");
-    printf("== 4 p/ Aparecer numero maior que 100           ==\n");
-    printf("== 5 p/ Cardápio de lanchonete                  ==\n");
-    printf("== 6 p/ Quadrado dos numeros inteiros            ==\n");
-    printf("== 7 p/ Consumo de combustivel                  ==\n");
-    printf("== 8 p/ Consumo de combustivel                  ==\n");
-    printf("== 9 p/ Consumo de combustivel                  ==\n");
-    printf("== 10 p/ Consumo de combustivel                 ==\n");
+    printf("== 01 p/ Lista 1 -> Exercicio 1                 ==\n");
+    printf("== 02 p/ Lista 1 -> Exercicio 4                 ==\n");
+    printf("== 03 p/ Lista 2 -> Exercicio 4                 ==\n");
+    printf("== 04 p/ Lista 2 -> Exercicio 14                ==\n");
+    printf("== 05 p/ Lista 3 -> Exercicio 4                 ==\n");
+    printf("== 06 p/ Lista 4 -> Exercicio 1                 ==\n");
+    printf("== 07 p/ Lista 6 -> Exercicio 3                 ==\n");
+    printf("== 08 p/ Lista 7 -> Exercicio 7                 ==\n");
+    printf("== 09 p/ Lista 8 -> Exercicio 1                 ==\n");
+    printf("== 10 p/ Lista 8 -> Exercicio 6                 ==\n");
     printf("==================================================\n\n");
     
-    printf("Digite o numero do exercicio de 1 a 10: ");
+    printf("Digite o numero do exercicio de 1 a 10: \n");
     scanf("%d", &escolha);
     
     system("cls || clear");
@@ -330,11 +482,11 @@ int main() {
         
         case 10:
             exercicio10();
-        
+            break;
         default:
-            printf("Opcao inválida, favor digitar corretamenta o numero de exercio de 1 a 10");
+            printf("\n\nOpcao invalida, favor digitar corretamenta o numero de exercio de 1 a 10\n\n");
+            return main();
             
     }
     return 0;
 }
-
